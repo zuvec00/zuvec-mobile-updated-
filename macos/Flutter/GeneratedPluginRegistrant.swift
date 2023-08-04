683e2c356647a321228a06554c3eeafb2c7d901c
Prince Ibekwe <zuvec00@gmail.com>
2023-08-04 12:34:54 +0100

    Upgraded the necessary packages in order to be compatabile with the latest version of flutter

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,7 +12,7 @@ import firebase_core
 import firebase_database
 import firebase_messaging
 import flutter_local_notifications
-import path_provider_macos
+import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
