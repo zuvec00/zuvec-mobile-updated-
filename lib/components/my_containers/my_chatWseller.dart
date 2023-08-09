d6518843c9595ce40f87ad0cc48ef7275296dfd1
Prince Ibekwe <zuvec00@gmail.com>
2023-08-09 09:18:16 +0100

    Changed the message to be sent when user taps on the chat with seller button

--- lib/components/my_containers/my_chatWseller.dart
+++ lib/components/my_containers/my_chatWseller.dart
@@ -3,14 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class MyChatWithSellerButton extends StatelessWidget {
-  const MyChatWithSellerButton({super.key});
+  final String productName;
+  final String productID;
+  final String productVariant;
+  final int quantity;
+
+  const MyChatWithSellerButton(
+      {super.key,
+      required this.productName,
+      required this.productID,
+      required this.productVariant,
+      required this.quantity});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
         launchUrl(Uri.parse(
-            'whatsapp://send?phone=+2348160287793&text=Hi, I would like to purchase '));
+            'whatsapp://send?phone=+2348160287793&text=Hello, I\'m interested in buying the product "$productName" (Product ID: $productID). I\'m looking for the "$productVariant" variant and a quantity of $quantity.'));
       },
       //radius: 25,
       splashColor: Colors.grey,
@@ -18,7 +28,7 @@ class MyChatWithSellerButton extends StatelessWidget {
         children: [
           Icon(FluentSystemIcons.ic_fluent_chat_filled,
               size: 18, color: Colors.deepPurple[600]),
-         const  SizedBox(
+          const SizedBox(
             width: 8,
           ),
           Text('Chat with seller',
